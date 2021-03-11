e2c6474005ff8a9fe4493063bb7f884804b039d3
Gregor Aitken <gregor.aitken@forwarddigital.co.uk>
2021-03-11 23:18:30 +0000

    adding Product Array

---
+++ checkout-kata/src/ProductArray.js
@@ -0,0 +1,35 @@
+const ProductData =  [
+  {
+    "name": "Apples",
+    "quantity": Number,
+    "price": 10,
+    "special": null
+    //special prices for individual Items
+  },
+  {
+    "name": "Pears",
+    "quantity": Number,
+    "price": 8,
+    "special": null
+  },
+  {
+    "name": "Oranges",
+    "quantity": Number,
+    "price": 26,
+    "special": 20
+  },
+  {
+    "name": "Kiwi",
+    "quantity": Number,
+    "price": 4,
+    "special": null
+  },
+  {
+    "name": "Grapes",
+    "quantity": Number,
+    "price": 7,
+    "special": null
+  }
+ ]
+
+ export default ProductData;
